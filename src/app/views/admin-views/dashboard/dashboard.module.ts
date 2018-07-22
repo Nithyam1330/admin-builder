6661379197cc9638f80a1102a25df9d2ea916883
nandhu <nanditha947@gmail.com>
2018-07-22 14:42:05 +0530

    Implemented Dashboard UI

--- src/app/views/admin-views/dashboard/dashboard.module.ts
+++ src/app/views/admin-views/dashboard/dashboard.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {FlexLayoutModule} from '@angular/flex-layout';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import {DashboardComponent} from '../dashboard/dashboard.component';
+import {MaterialModule} from '../../../layout/material/material.module';
 @NgModule({
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   declarations: [
     DashboardComponent
